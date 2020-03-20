7f6169dac8a9cba1bb7fbd9262fedf67d7946c24
somethiiing <hello@wilsonyu.io>
2020-03-19 17:53:51 -0700

    save this shit

--- server.js
+++ server.js
@@ -1,46 +1,34 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-const port = 3000;
-
 app.use(express.static(path.join(__dirname, 'build')));
 
 let state = {
   successCount: 0,
   failCount: 0
-};
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
-app.get('/getData', (req, res) => {
-  res.send({status: 'DATA_SENT', state});
-});
-
 app.get('/vote/success', (req, res) => {
   state.successCount = state.successCount + 1;
   res.send({status: 'VOTE_REGISTERED'});
-})
+});
 
-app.get('/vote/fail', (req, res) => {
+app.get('/vote/reject', (req, res) => {
   state.failCount = state.failCount + 1;
   res.send({status: 'VOTE_REGISTERED'});
-});
+})
 
 app.get('/reset', (req, res) => {
   state = {
     successCount: 0,
     failCount: 0
-  };
+  }
   res.send({status: 'STATE_RESET'});
-});
+})
 
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-app.listen(port, () => console.log(`listening on port: ${port}`))
+app.listen(process.env.PORT || 8080);
