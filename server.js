0a47fd3b524fa3f4d85c734c4ea0a09abe55536a
somethiiing <hello@wilsonyu.io>
2020-03-19 18:08:14 -0700

    update port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -38,4 +38,4 @@ app.get('/reset', (req, res) => {
   res.send({status: 'STATE_RESET'});
 })
 
-app.listen(port, () => console.log('listening on port: 3000'));
+app.listen(PORT, () => console.log('listening on port: 3000'));
