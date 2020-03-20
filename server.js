4747b60aa12b0ca47c46be3aa18cd003643eec88
somethiiing <hello@wilsonyu.io>
2020-03-19 17:58:15 -0700

    update

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+const port = 3000;
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 let state = {
@@ -9,16 +11,20 @@ let state = {
   failCount: 0
 }
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.get('/getData', (req, res) => {
+  res.send({status: 'DATA_SENT', state})
+})
 
 app.get('/vote/success', (req, res) => {
   state.successCount = state.successCount + 1;
   res.send({status: 'VOTE_REGISTERED'});
 });
 
-app.get('/vote/reject', (req, res) => {
+app.get('/vote/fail', (req, res) => {
   state.failCount = state.failCount + 1;
   res.send({status: 'VOTE_REGISTERED'});
 })
@@ -31,4 +37,4 @@ app.get('/reset', (req, res) => {
   res.send({status: 'STATE_RESET'});
 })
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => console.log('listening on port: 3000'));
